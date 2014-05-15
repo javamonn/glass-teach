0585f441bee39d4fc036cd73cca012d18220b4d8
Daniel Ramirez <javamonn@gmail.com>
2014-05-15 08:48:10 -0500

    Began work on phone <-> computer link

--- glass-teach-phone/glass-teach-phone/src/main/java/com/glass_teach_phone/glass_teach_phone/MainActivity.java
+++ glass-teach-phone/glass-teach-phone/src/main/java/com/glass_teach_phone/glass_teach_phone/MainActivity.java
@@ -1,23 +1,42 @@
 package com.glass_teach_phone.glass_teach_phone;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    Button monitor;
+    Button monitorButton;
+    ProgressBar progressBar;
+    TextView helpText;
+    TextView connectText;
+
+    ServerSocket serverSocket;
+    Socket clientSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.spinner);
+        helpText = (TextView) findViewById(R.id.help_text);
+        connectText = (TextView) findViewById(R.id.connect_text);
+        monitorButton = (Button) findViewById(R.id.monitor);
+        monitorButton.setOnClickListener(this);
 
-        monitor = (Button) findViewById(R.id.monitor);
-        monitor.setOnClickListener(this);
+        //attempt to connect
+
+        Connect task = new Connect();
     }
 
     @Override
@@ -32,6 +51,34 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     //send turn on command to host computer
                     view.setTag("on");
                 }
+        }
+    }
+
+    private class Connect extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                //port 38300 (arbitrary)
+                serverSocket = new ServerSocket(38300);
+                serverSocket.setSoTimeout(0);
+                // attempt to connect, infinite time out
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(Void voids) {
+            //if we reach here a connection has been established so we update UI
+            helpText.setVisibility(View.GONE);
+            connectText.setVisibility(View.GONE);
+            progressBar.setVisibility(View.GONE);
+            monitorButton.setVisibility(View.VISIBLE);
+
+            //set connection flag so main UI knows we're ready
         }
     }
 }
