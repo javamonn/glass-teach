9865c9f227450f3512952e3e83282a52864acc9b
Daniel Ramirez <javamonn@gmail.com>
2014-05-21 08:43:49 -0500

    configured auto-start, application hiding

--- glass-teach-client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def glass_teach_client():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('ec2-54-187-236-208.us-west-2.compute.amazonaws.com', 8080))
-    print('connected')
-
-if __name__ == '__main__':
-    glass_teach_client()
---
+++ student-init-hide.vbs
@@ -0,0 +1,2 @@
+'place at C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+CreateObject("Wscript.Shell").Run "C:\Users\ramir292035\Documents\GitHub\glass-teach\student-init.bat", 0, True
